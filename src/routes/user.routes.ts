12300c25d3370743b7b477838cbea8c00d32267b
maniram <mani.ram@ditstek.com>
2024-05-14 11:31:12 +0530

    remove auth from create user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import UserController from './../features/user/user.controller';
 import { auth, validateId } from '../middleware/index';
 const router = express.Router();
-router.post('/', auth, UserController.create);
+router.post('/', UserController.create);
 router.get('/:id', auth, validateId, UserController.findOne);
 router.get('/', auth, UserController.findAll);
 router.post('/update/:id', auth, UserController.update);
